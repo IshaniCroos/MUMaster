5ccc5891d3fbe9d609c9fd5b7d5b563a17457a4d
Dharshi Gunawardana <33058316+Dharshi-Gunawardana@users.noreply.github.com>
2019-05-02 11:59:52 +0530

    Add files via upload

--- MUMasterApp/Routes.js
+++ MUMasterApp/Routes.js
@@ -1,12 +1,13 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-import{Router,Stack,Scene} from 'react-native-router-flux';
+import { Router, Stack, Scene } from 'react-native-router-flux';
 
 
 import Register from './Register';
 import LoginPage from './Login';
 import Main from './Main';
 import MUMain from './MUMain';
+import SettingsPage from './SettingsPage';
 
 export default class Routes extends Component {
     render() {
@@ -16,13 +17,15 @@ export default class Routes extends Component {
             <Router>
 
                 <Stack key="root" hideNavBar={true}>
-                <Scene key="main" component={Main} title="Main" initial={true}></Scene>
-                <Scene key="register" component={Register} title="Register" ></Scene>
-                <Scene key="login" component={LoginPage} title="LoginPage"></Scene>
-                <Scene key="mumain" component={MUMain} title="MUMainPage"> </Scene>
+                    <Scene key="main" component={Main} title="Main" initial={true}></Scene>
+                    <Scene key="register" component={Register} title="Register" ></Scene>
+                    <Scene key="login" component={LoginPage} title="LoginPage"></Scene>
+                    <Scene key="mumain" component={MUMain} title="MUMainPage"> </Scene>
+
+                    <Scene key="settingsPage" component={SettingsPage} title="SettingsPage"> </Scene>
 
                 </Stack>
-</Router>
+            </Router>
 
 
 
