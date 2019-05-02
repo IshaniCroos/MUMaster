ece3ba5ad11292795a4d1e27ad180bb3559307df
Dharshi Gunawardana <33058316+Dharshi-Gunawardana@users.noreply.github.com>
2019-05-02 11:57:26 +0530

    Add files via upload

--- MUMasterApp/MUMain.js
+++ MUMasterApp/MUMain.js
@@ -1,68 +1,85 @@
 import React from 'react';
-import { 
-    StyleSheet,
-    Text,
-    View,
-    Dimensions,
-    TextInput,
-    TouchableOpacity,
-    ImageBackground,
-    Image,
-    
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+
 } from 'react-native';
 
 import bgImage from './images/background7.jpg'
 import logo from './images/logo.jpg'
-
-const {width : WIDTH}= Dimensions.get('window')
+import { Actions } from 'react-native-router-flux';
+const { width: WIDTH } = Dimensions.get('window')
 export default class Login extends React.Component {
 
-render(){
+  SettingsPage() {
+    Actions.settingsPage() //method to get another page via routing
+  }
 
-return(
+  render() {
 
-    <ImageBackground source={bgImage} style={styles.container}>
-      <View style= {styles.overlayContainer}>
+    return (
 
-        <View style={styles.logoContainer}>
-          <Image source={logo} style={styles.logo}   />
-          <Text style={styles.logoText}> M U - M A S T E R </Text>
+      <ImageBackground source={bgImage} style={styles.container}>
+        <View style={styles.overlayContainer}>
+
+          <View style={styles.logoContainer}>
+
+            <Image source={logo} style={styles.logo} />
+            <Text style={styles.logoText}> M U - M A S T E R </Text>
+          </View>
+
+          <TouchableOpacity onPress={this.SettingsPage}>
+            <Text> Settings</Text>
+          </TouchableOpacity>
+          <Text style={styles.texts}> Hi Welcome To Our Main Page!</Text>
+
+
+
+
+
+
+
         </View>
 
-        <Text style={styles.texts}> Hi Welcome To Our Main Page!</Text>
-
-
-
-    </View>
-    </ImageBackground>
 
 
 
 
-);
+      </ImageBackground>
 
-}
+
+
+
+    );
+
+  }
 
 }
 
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        width: null,
-        height: null,
-        //backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-  overlayContainer:{
+  container: {
+    flex: 1,
+    width: null,
+    height: null,
+    //backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  overlayContainer: {
 
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.5)',
     width: '100%',
     height: '100%'
   },
-  logoContainer:{ 
+  logoContainer: {
     alignItems: 'center',
     marginBottom: 50,
     padding: 20
@@ -70,17 +87,17 @@ const styles = StyleSheet.create({
 
   logo: {
     width: 120,
-    height:120
+    height: 120
   },
 
   logoText: {
-    width: WIDTH -55,
+    width: WIDTH - 55,
     color: '#4B0082',
     fontSize: 28,
     borderColor: '#fff',
     borderWidth: 2,
     padding: 20,
-    paddingLeft:40,
+    paddingLeft: 40,
     paddingRight: 40,
     backgroundColor: 'rgba(255,255,255, .5)',
     fontWeight: '500',
@@ -89,8 +106,8 @@ const styles = StyleSheet.create({
 
   },
 
-  texts:{
-    flexGrow:1,
+  texts: {
+    flexGrow: 1,
     alignItems: 'flex-end',
     justifyContent: 'center',
     paddingVertical: 16,
