5fe06d3939ea9c2dc0cb2bbbd2be3a3306bad73c
Haripriya Cumaraswamy <40045897+echpee@users.noreply.github.com>
2019-05-05 02:53:03 +0530

    Update NavigationBar.js

--- MUMasterApp/NavigationBar.js
+++ MUMasterApp/NavigationBar.js
@@ -27,7 +27,7 @@ class NavigationDrawerStructure extends Component {
         <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
           {}
           <Image
-            source={require('./image/drawer.png')}
+            source={require('./images/drawer.png')}
             style={{ width: 25, height: 25, marginLeft: 5 }}
           />
         </TouchableOpacity>
